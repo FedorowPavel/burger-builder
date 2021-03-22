84db75c3ba9172ef69f742be79881954bb13034c
FedorowPavel <pasha_fedorow@mail.ru>
2021-03-22 13:27:42 +0200

    sideDrawer toggle

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -1,20 +1,35 @@
 import React from 'react';
+import Aux from '../../../hoc/Auxillary';
 import Logo from '../../Logo/Logo';
+import Backdrop from '../../UI/Backdrop/Backdrop';
 import NavigationItems from '../NavigationItems/NavigationItems';
-import classes from './SideDrawer.css'
+import classes from './SideDrawer.css';
 
 const sideDrawer = (props) => {
 
 
+    let attachedClasses = [classes.SideDrawer, classes.Close];
+    
+    if (props.open) {
+        attachedClasses = [classes.SideDrawer, classes.Open];
+    }
     
     return (
-        <div className={classes.sideDrawer}>
-            <Logo height='11%'/>
-            <nav>
-                <NavigationItems/>
-            </nav>
-            
-        </div>
+        <Aux>
+            <Backdrop show={props.open} clicked={ props.closed }/>
+            <div className={attachedClasses.join(' ')}>
+                
+
+                <div className={classes.Logo}>
+                    <Logo />
+                </div>
+
+                <nav>
+                    <NavigationItems/>
+                </nav>
+                
+            </div>
+        </Aux>
     )
 }
 
