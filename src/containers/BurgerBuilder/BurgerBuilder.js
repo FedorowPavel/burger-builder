90f96ef7627c632e87eba23da05547f3b4716922
FedorowPavel <pasha_fedorow@mail.ru>
2021-03-23 12:49:07 +0200

    axios post orders method implemented

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,7 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BulidControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import axios from '../../axios-orders'
 
 const INGREDIENTS_PRICES = {
     salad: 0.5,
@@ -87,7 +88,24 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-        alert('You continue!')
+        // alert('You continue!')
+        const order = {
+            ingredients: this.state.ingredients,
+            price: this.state.totalPrice,
+            customer: {
+                name: "Pavel Fedorow",
+                address: {
+                    street: 'Folush',
+                    zipCode: '230006',
+                    country: 'Belarus'
+                },
+                email: 'text@mail.com',
+            },
+            deliveryMethod: 'fastest'
+        }
+        axios.post('/orders.json', order)
+            .then(response => console.log(response))
+            .catch(error=> console.log(error))
     }
 
 
