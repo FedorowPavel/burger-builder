be6d17814f09bc6c2746ac67f5e572fe4bc21db7
FedorowPavel <pasha_fedorow@mail.ru>
2021-03-25 13:43:12 +0200

    checkout order rederect to main page

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -99,34 +99,15 @@ class BurgerBuilder extends Component {
 
     purchaseContinueHandler = () => {
         // alert('You continue!')
-    //     this.setState({loading: true})
-    //     const order = {
-    //         ingredients: this.state.ingredients,
-    //         price: this.state.totalPrice,
-    //         customer: {
-    //             name: "Pavel Fedorow",
-    //             address: {
-    //                 street: 'Folush',
-    //                 zipCode: '230006',
-    //                 country: 'Belarus'
-    //             },
-    //             email: 'text@mail.com',
-    //         },
-    //         deliveryMethod: 'fastest'
-    //     }
-    //     axios.post('/orders.json', order)
-    //         .then(response => {
-    //             this.setState({loading: false, purchasing: false})
-    //         })
-    //         .catch(error => {
-    //             this.setState({loading: false, purchasing: false})
-    //         })
+   
         const queryParams = [];
         console.log(this.state.ingredients);
 
         for (let i in this.state.ingredients) {
             queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]))
         }
+
+        queryParams.push('price=' + this.state.totalPrice)
 
         console.log(queryParams);
         const queryString = queryParams.join('&');
