dc40f56ca5f4c38627f02fc3ebd26f0b0381af65
FedorowPavel <pasha_fedorow@mail.ru>
2021-03-26 13:42:05 +0200

    custom inputs components added and styled

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -3,6 +3,8 @@ import React, { Component } from "react";
 import Button from "../../../components/UI/Button/Button";
 import classes from './ContactData.css';
 import Spinner from '../../../components/UI/Spinner/Spinner';
+import Input from '../../../components/UI/Input/Input';
+
 
 class ContactData extends Component {
     state = {
@@ -43,13 +45,15 @@ class ContactData extends Component {
     }
 
     render() {
-        let form = (<form>
-                    <input className={ classes.Input } type='text' name='name' placeholder='Your Name'></input>
-                    <input className={ classes.Input } type='email' name='email' placeholder='Your Mail'></input>
-                    <input className={ classes.Input } type='text' name='street' placeholder='Street'></input>
-                    <input className={ classes.Input } type='text' name='postal' placeholder='Postal Code'></input>
+        let form = (
+            <form>
+                    <Input inputtype='input' type='text' name='name' placeholder='Your Name'></Input>
+                    <Input inputtype='input' type='email' name='email' placeholder='Your Mail'></Input>
+                    <Input inputtype='input' type='text' name='street' placeholder='Street'></Input>
+                    <Input inputtype='input' type='text' name='postal' placeholder='Postal Code'></Input>
                     <Button btnType='Success' clicked={this.orderHandler}>Order</Button>
-                </form>);
+            </form>
+        );
         if (this.state.loading) {
             form = <Spinner/>
         }
