bf02f91f51b0b43bc24cc999d599243f4e768c86
FedorowPavel <pasha_fedorow@mail.ru>
2021-04-02 08:45:04 +0200

    async init ingridients

--- src/store/reducers/burgerBuilder.js
+++ src/store/reducers/burgerBuilder.js
@@ -33,6 +33,17 @@ const reducer = (state = initialState, action) => {
                 },
                 totalPrice: state.totalPrice - INGREDIENTS_PRICES[action.ingredientName]
             }
+        case actionTypes.SET_INGREDIENTS:
+            return {
+                ...state,
+                ingredients: action.ingredients,
+                error: false
+            }
+        case actionTypes.FETCH_INGREDIENTS_FALED:
+            return {
+                ...state,
+                error: true
+            }
         default:
             return state;
     }
